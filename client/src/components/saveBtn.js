034973db237e6ab48b283fa2accea47a935ef20d
Jericho Ronquillo <jericho1010761@gmail.com>
2021-11-24 18:49:50 -0800

    merging

---
+++ client/src/components/saveBtn.js
@@ -0,0 +1,16 @@
+import { cloneElement } from "react";
+
+const searchButton = search.querySelector('button')
+
+document.addEventListener('click', () => {
+    let data = {
+        foodList
+    }
+
+    localStorage.setItem('user-saves', JSON.stringify(data))
+
+
+})
+
+    
+   
