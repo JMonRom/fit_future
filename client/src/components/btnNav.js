116acc522fa71d9832ae2eebcd7e53e5ee3b3745
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-29 19:17:33 -0800

    Structure

--- client/src/components/btnNav.js
+++ client/src/components/btnNav.js
@@ -20,7 +20,30 @@ morningBtn = (e) => {
     let data = JSON.parse(localStorage.getItem("morning")).class
     const newDiv = document.createElement('div')
     document.body.appendChild(newDiv)
-    console.log('hello')
-
-    
+    console.log('hello')    
 }
+
+// import { useState } from 'react';
+// import ReactDom from 'react-dom'
+
+
+// function BreakfastForm() {
+//     const [food, setFood] = useState('');
+//     // Local storage items here
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         // append{food} to <p> tag
+//         // Save the food inputs
+//     }
+
+//     return (
+//         <form>
+//         {/* breakfast input type = text */}
+//         {/* Onchange= {e} => setFood(e.target.value) */}
+//         {/* {input type submit button } */}
+
+//         </form>
+//     )
+
+// }
