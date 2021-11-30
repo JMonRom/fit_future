33995170bca6b2c657bd265847cae5c224988952
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-29 21:24:02 -0800

    Setting up save button

--- client/src/components/addBtn.js
+++ client/src/components/addBtn.js
@@ -9,6 +9,51 @@ const addFood = (ev) => {
         protienCount: document.getElementById('proteinCount').value,
         fatCount: document.getElementById('fatCount').value,
         caloriesCount: document.getElementById('caloriesCount').value,
-        
     }
 }
+
+import { useState, useRef } from 'react';
+import ReactDom from 'react-dom';
+// import api from "./apifolder"
+
+function addFood() {
+    const [food, setFood] = useState //('REST API keys of id, sring, number of etc')
+    const foodRef = useRef() //Allows access to the input so that it knows we are using it
+
+    function handleFood(e) {
+        const saveFood = foodRef.current.value
+        if (saveFood === '') return //If there is no food input just return
+        setFood([]) //clears out all Food items
+    }
+
+    return (
+        <>
+        <input ref={foodRef}type="text" />
+        <button onClick={handleFood}>Add Food</button>
+        
+        </>
+    )
+}
+
+
+
+// function BreakfastForm() {
+//     const [food, setFood] = useState('');
+//     // Local storage items here
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         // append{food} to <p> tag
+//         // Save the food inputs
+//     }
+
+//     return (
+//         <form>
+//         {/* breakfast input type = text */}
+//         {/* Onchange= {e} => setFood(e.target.value) */}
+//         {/* {input type submit button } */}
+
+//         </form>
+//     )
+
+// }
