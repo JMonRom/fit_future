ab30c84a8011641ca8eea0a2919a3eee7d88bf5c
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-12-01 13:54:10 -0800

    worked on saved food and search food js and fixing any missing connections, ran into API error

--- client/src/pages/SearchFoods.js
+++ client/src/pages/SearchFoods.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-import Auth from '../utils';
+import Auth from '../utils/auth';
 import { saveFoodIds, getSavedFoodIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/client';
 import { SAVE_FOOD } from '../utils/mutations';
